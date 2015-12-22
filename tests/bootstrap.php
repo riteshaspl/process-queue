a6f4221fe4372a46df16125da20cb2510c17dd87
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-12-22 19:51:10 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,7 +14,7 @@
  * @subPackage Tests
  */
 call_user_func(function() {
-    if ( ! is_file($autoloadFile = realpath(dirname(__FILE__).'/../vendor/autoload.php'))) {
+    if (!is_file($autoloadFile = realpath(dirname(__FILE__) . '/../vendor/autoload.php'))) {
         throw new \RuntimeException('Did not find vendor/autoload.php. Did you run "composer install --dev"?');
     }
 
